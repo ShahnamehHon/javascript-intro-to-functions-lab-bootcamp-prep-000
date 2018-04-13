6fcdcc0c1f763f66276943ce4a4a946ce3abf564
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-13 15:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function logWhisper(string) {
   console.log('HELLO'.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if ((string)===uppercase) {
-    return 'YES INDEED!'
-  }
-  else if ((string)===lowercase) {
+    if ((string)===lowercase) {
     return "I can't hear you!"
+  }
+  else if ((string)===uppercase) {
+    return 'YES INDEED!'
   }
   else if ((string)===("I love you, Grandma.")) {
     return "I love you, too."
